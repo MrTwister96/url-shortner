ec479d041b1527e8439ad826e1c059eb3062f04a
Schalk Olivier <schalkolivier@vegamine.com>
2021-04-24 20:35:40 +0200

    Added Base Button Component

---
+++ src/components/buttons/BaseButton.vue
@@ -0,0 +1,13 @@
+<template>
+	<button
+		class="flex px-5 py-2 bg-gradient-to-r from-blue-500 to-blue-400 font-semibold text-white transform hover:scale-105 hover:shadow transition duration-75 ease-in-out rounded-md"
+	>
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+export default {
+	name: "base-button",
+};
+</script>
