171f15567ce638b7669ca7f10ca987a62ded4888
Schalk Olivier <schalkolivier@vegamine.com>
2021-04-24 21:46:50 +0200

    Added BaseInput Component and updated BaseButton

---
+++ src/components/inputs/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="base-input shadow flex">
+		<input
+			@input="handleInput"
+			:type="type"
+			:placeholder="placeholder"
+			class="w-full p-2 focus:outline-none rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-gray-500 focus:placeholder-gray-300"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		type: { required: false, default: "text" },
+		placeholder: { required: false, default: "" },
+		modelValue: String,
+	},
+	setup(props, { emit }) {
+		function handleInput(event) {
+			emit("update:modelValue", event.target.value);
+		}
+
+		return { handleInput };
+	},
+};
+</script>
