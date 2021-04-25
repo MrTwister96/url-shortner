b58b00e56be383d01945f6fddec1db0bf1768c1b
Schalk Olivier <schalkolivier@vegamine.com>
2021-04-25 23:46:27 +0200

    Created Componenets for current home page

---
+++ src/components/pages/homepage/HomeLayout.vue
@@ -0,0 +1,11 @@
+<template>
+	<div
+		class="flex flex-col space-y-14 md:p-20 lg:p-40 min-h-screen bg-gradient-to-tr from-indigo-500 to-green-400"
+	>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
